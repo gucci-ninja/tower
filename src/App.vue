d6d82aa5d5fb2076456c06b67d247d2d8e4d654d
Wayne Ferrao <wayne.ferrao93@gmail.com>
2020-07-21 01:06:11 -0400

    Added font & finished responsive Landing page.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LandingPage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './components/LandingPage'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LandingPage
   }
 }
 </script>
 
 <style>
+body, html {
+  height: 100%;
+  background-color: #F6F6F2;
+  font-family: 'Quicksand', sans-serif;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+@media only screen and (min-width: 768px) {
+  #app {
+    /* margin-top: 60px; */
+
+  }
 }
 </style>
