8f060a795972f2409a6acc1ed2dea327cd513681
gucci-ninja <beautyspell5@gmail.com>
2020-07-22 00:16:18 -0400

    added sockets

--- server/index.js
+++ server/index.js
@@ -1,45 +1,31 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app)
-const io = require('socket.io')(server);
 const { Nuxt, Builder } = require('nuxt');
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
 const config = require('../nuxt.config.js');
 
 var users = 0;
-var user_names = []
 
-io.on('connection', (socket) => {
-    console.log(socket.id);
-    console.log("socket in");
-    user_names.push(socket.id);
-    io.sockets.emit('users', {
-        users: ++users,
-        user_names: user_names
-    });
+io.on("connection", (socket) => {
+  socket.on("addUser", () => {
+    io.emit("updateUsers", ++users);
+  });
 
-    socket.on('disconnect', () => {
-        user_names.splice(user_names.indexOf(socket.id), 1);
-        io.sockets.emit('users', {
-            users: --users,
-            user_names: user_names
-        });
-    });
+  socket.on("disconnect", () => {
+    io.emit("updateUsers", --users)
+  });
 });
 
 async function start() {
-    const nuxt = new Nuxt(config);
+  const nuxt = new Nuxt(config);
+  const { host, port } = nuxt.options.server;
+  const builder = new Builder(nuxt);
+  await builder.build();
+  app.use(nuxt.render);
 
-    const { host, port } = nuxt.options.server;
-
-    const builder = new Builder(nuxt);
-    await builder.build();
-
-    app.use(nuxt.render);
-
-    server.listen(port, () => {
-        console.log('running on a port lol');
-    })
+  server.listen(port, () => {
+    console.log(`Server listening on http://${host}:${port}`)
+  });
 }
-
 start();
