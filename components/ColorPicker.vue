fb57d93f7acd0715a717c96419baa9793b741146
gucci-ninja <beautyspell5@gmail.com>
2020-08-30 00:55:07 -0400

    added constraints from dragging and click-away for color-picker

--- components/ColorPicker.vue
+++ components/ColorPicker.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-card color="#EEEEEE">
+  <v-overlay
+    :absolute="absolute"
+    :value="overlay"
+    :opacity="0"
+  >
+    <v-btn
+      v-for="color in colors"
+      :key="color"
+      :color="color"
+      @click="colorPicked(color)"
+      icon>
+      <v-icon>mdi-circle</v-icon>
+    </v-btn>
+  </v-overlay>
 
-  <v-btn
-    v-for="color in colors"
-    :key="color"
-    :color="color"
-    @click="colorPicked(color)"
-    icon>
-    <v-icon>mdi-circle</v-icon>
-  </v-btn>
-  <v-btn icon @click="done">
-    <v-icon>mdi-check</v-icon>
-  </v-btn>
-
-</v-card>
 </template>
 
 <script>
@@ -34,12 +34,9 @@ export default {
       this.selected = color;
       this.$emit('color-picked', this.selected);
     },
-    done() {
-      this.$emit('done');
-    }
   },
   mounted() {
     this.selected = this.defaultColor;
-  }
+  },
 }
 </script>
