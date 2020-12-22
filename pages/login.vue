1512712eb3db57f2da248f0173fd6301d0d0ffb8
gucci-ninja <beautyspell5@gmail.com>
2020-12-22 01:24:09 -0500

    Separate page for authentication

--- pages/login.vue
+++ pages/login.vue
@@ -1,30 +1,39 @@
 <template>
   <div id="app">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap" rel="stylesheet">
-      <LandingPage/>
+      <LoginForm/>
+
   </div>
 </template>
 
 <script>
+import LoginForm from '../components/LoginForm.vue';
 import LandingPage from '../components/LandingPage.vue';
-
 
 export default {
   name: 'App',
   components: {
-    LandingPage,
+    LoginForm,
+    LandingPage
   },
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
 body, html {
-  height: 100%;
-  background-color: #F6F6F2;
+  width: 100%;
+  min-height: 100vh;
+  background-color: black;
   font-family: 'Quicksand', sans-serif;
   text-align: center;
 }
+
+
 #app {
+  width: 100%;
+  min-height: 100vh;
+  background-color: #C7CEEA;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -37,4 +46,5 @@ body, html {
 
   }
 }
+
 </style>
